dde25f10aa73ed1f930f64ddf5824ddf6c43c91b
Camille Paquet <camille.paquet@yahoo.com>
2022-03-03 21:05:19 +0100

    style: 💄 ajout style detail recette

--- src/components/DetailsRecipe.style.js
+++ src/components/DetailsRecipe.style.js
@@ -1,5 +1,26 @@
 import { createUseStyles } from "react-jss";
 
-const useStyles = createUseStyles({});
+const useStyles = createUseStyles({
+  ingredientsContainer: {
+    position: "relative",
+  },
+  leftContainer: {
+    background: "#baceb3",
+    margin: "auto",
+    position: "absolute",
+    top: "10px",
+    left: "15%",
+    bottom: "13px",
+    width: "35%",
+  },
+  title: {
+    color: "white",
+    margin: 0,
+  },
+  rightContainer: {
+    textAlign: "right",
+    margin: "5% 15%",
+  },
+});
 
 export default useStyles;
