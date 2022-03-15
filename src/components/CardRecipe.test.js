9fb2535f1a4d06c8d0b167d78f99e6ed8707dfb7
Camille Paquet <camille.paquet@yahoo.com>
2022-03-15 22:55:14 +0100

    test: 💍 ajout test pour la carte d'une recette

--- src/components/CardRecipe.test.js
+++ src/components/CardRecipe.test.js
@@ -1,18 +1,19 @@
 import { screen, render } from "@testing-library/react";
-import ButtonLike from "./ButtonLike";
+import CardRecipe from "./CardRecipe";
 import recipe from "../data_recipe.json";
+import App from "../App";
 import { Provider } from "react-redux";
 import configureStore from "redux-mock-store";
-
-describe("ButtonLike", () => {
-  it("render a button to like", () => {
+describe("CardRecipe", () => {
+  it("renders nothing without `id`", () => {
     const initialState = { favorites: recipe };
     const mockStore = configureStore();
     render(
       <Provider store={mockStore(initialState)}>
-        <ButtonLike recipe={recipe} text="Like" />
+        <CardRecipe recipe={recipe} index={2} />
       </Provider>
     );
-    expect(screen.getByRole("button")).toBeInTheDocument();
+    expect(screen.getByRole("img")).toBeInTheDocument();
+    expect(screen.getByText("Bacon Wrapped Breadsticks")).toBeInTheDocument();
   });
 });
